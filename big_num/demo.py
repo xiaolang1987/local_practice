2c472c1dc3863efa90120b8469c90f542fc534ac
zhaopeng <zhao19870908@qq.com>
2022-10-31 00:37:08 +0800

    初步增加读的部分

--- big_num/demo.py
+++ big_num/demo.py
@@ -3,3 +3,8 @@
 # author@zhaopeng
 # @time: 2022/10/27 23:41
 
+from openpyxl import load_workbook
+
+wb = load_workbook('./demo.xlsx')
+# https://zhuanlan.zhihu.com/p/409954748
+print(wb.sheetnames)
