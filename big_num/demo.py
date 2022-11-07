5ce16a48a3f68553e010a4f7cf88d138076dd4ef
zhaopeng <zhaopeng@zhaopengdeMacBook-Pro.local>
2022-11-07 20:34:04 +0800

    更新部分读表格

--- big_num/demo.py
+++ big_num/demo.py
@@ -5,6 +5,20 @@
 
 from openpyxl import load_workbook
 
-wb = load_workbook('./demo.xlsx')
 # https://zhuanlan.zhihu.com/p/409954748
+# https://geek-docs.com/python/python-tutorial/python-openpyxl.html
+'''
+读取excel信息
+'''
+wb = load_workbook('./demo.xlsx')
+# 获取sheet列表
 print(wb.sheetnames)
+
+# 加载sheet
+ws = wb["表三"]
+# 读取单个单元格
+print(ws['A1'].value)
+# 读取多个单元格
+calls = ws['B4': 'N21']
+for i, j in calls:
+    print(i.value, j.value)
