136c6aab65808c9e1da6b062f6ab3b6e3358782d
xiaolang <zhao19870908@qq.com>
2023-07-30 00:08:03 +0800

    http请求升级成类

--- download_files/utils.py
+++ download_files/utils.py
@@ -8,51 +8,42 @@ from urllib3.exceptions import InsecureRequestWarning
 from urllib3 import disable_warnings
 import random
 import time
+
 # 禁用安全请求警告
 disable_warnings(InsecureRequestWarning)
 
-def post(url, data=None, cookie=None, header=None, is_json=False, files=None, params=None):
-    data = data if data else {}
-    cookie = cookie if cookie else {}
-    header = header if header else {"Connection": "close"}
-    files = files if files else {}
-    params = params if params else {}
-    try:
-        # post_start_time = time.time()
-        if is_json:
-            res = requests.post(url, json=data, cookies=cookie, headers=header, verify=False, files=files,
-                           params=params, timeout=10)
-        else:
-            res = requests.post(url, data=data, cookies=cookie, headers=header, verify=False, files=files,
-                           params=params, timeout=10)
-        # post_end_time = time.time()
-        # post_time_used = post_end_time - post_start_time
-        # print("\n【耗时：%s。接口返回数据：%s】" % (post_time_used, res))
 
-    except Exception as e:
-        res = {"error": str(e)}  # 如果接口调用出错的话，那么就返回一个有错误信息的字典
-        print("\n异常信息：接口调用失败！ url 【%s】 data 【%s】 实际结果是 【%s】" % (url, data, res))
+class req:
+    ot = 10
+    verify = False
 
-    return res
+    def __init__(self, url, params=None, header=None, payload=None, files=None, cookie=None):
+        self.url = url
+        self.params = params if params else {}
+        self.header = header if header else {"Connection": "close"}
+        self.payload = payload if payload else {}
+        self.files = files if files else {}
+        self.cookie = cookie if cookie else {}
 
+    def get(self):
+        try:
+            res = requests.get(self.url, params=self.params, headers=self.header, data=self.payload,
+                               cookies=self.cookie, verify=req.verify, timeout=req.ot)
+        except Exception as e:
+            res = {"error": str(e)}  # 如果接口调用出错的话，那么就返回一个有错误信息的字典
+            print("异常信息：接口调用失败！ url 【%s】 data 【%s】 实际结果是 【%s】" % (self.url, self.params, res))
 
-def get(url, data=None, cookie=None, header=None):
-    data = data if data else {}
-    data.update({"v": "istest_0.%s" % str(random.randint(0, 1000000)).zfill(7)})
-    cookie = cookie if cookie else {}
-    header = header if header else {}
-    # print("【请求接口地址：%s】" % url)
-    # print("【接口请求数据：%s】" % data)
-    try:
-        get_start_time = time.time()
-        res = requests.get(url, params=data, cookies=cookie, headers=header, verify=False, timeout=10)
-        # get_end_time = time.time()
-        # get_time_used = get_end_time - get_start_time
-        # print("【耗时：%s。接口返回数据：%s】" % (get_time_used, res))
-    except Exception as e:
-        res = {"error": str(e)}  # 如果接口调用出错的话，那么就返回一个有错误信息的字典
-        print("异常信息：接口调用失败！ url 【%s】 data 【%s】 实际结果是 【%s】" % (url, data, res))
-    return res
+        return res
+
+    def post(self):
+        try:
+            res = requests.post(self.url, params=self.params, headers=self.header, data=self.payload, files=self.files,
+                                cookies=self.cookie, verify=req.verify, timeout=req.ot)
+        except Exception as e:
+            res = {"error": str(e)}  # 如果接口调用出错的话，那么就返回一个有错误信息的字典
+            print("\n异常信息：接口调用失败！ url 【%s】 data 【%s】 实际结果是 【%s】" % (self.url, self.params, res))
+
+        return res
 
 
 def down_file(down_url, file_name):
