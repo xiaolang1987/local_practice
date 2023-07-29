5c8ffdf747f2513c61e6779aa4a223f11ada621b
xiaolang <zhao19870908@qq.com>
2023-07-30 00:09:41 +0800

    http请求改用类，bin文件更新

--- download_files/bin.py
+++ download_files/bin.py
@@ -3,6 +3,14 @@
 # author@zhaopeng
 # @time: 2023/6/9 09:52
 
-import utils
+import draft
 
-utils.down_file("a", "a")
+bili = draft.bili_station()
+bili.get_av_list()
+
+# 对比需要下载的内从
+# lista = ["a", "b", "d"]
+# listb = ["a"]
+# print(set(lista).difference(set(listb)))
+
+# 开始下载
