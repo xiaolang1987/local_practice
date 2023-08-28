58021b1ef73695871faef44ad7abc7c81a129922
zhaopeng <zhao19870908@qq.com>
2023-08-28 20:39:57 +0800

    公共请求方法里增加随机user_agent

--- download_files/utils.py
+++ download_files/utils.py
@@ -16,11 +16,21 @@ disable_warnings(InsecureRequestWarning)
 class req:
     ot = 10
     verify = False
+    user_agent_list = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3 Edge/16.16299",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36 Edge/19.6",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.2 Safari/605.1.15",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
+    ]
 
     def __init__(self, url, params=None, header=None, payload=None, files=None, cookie=None):
         self.url = url
         self.params = params if params else {}
-        self.header = header if header else {"Connection": "close"}
+        self.header = header if header else {}
+        self.header["User-Agent"] = req.user_agent_list[random.randrange(len(req.user_agent_list))]
         self.payload = payload if payload else {}
         self.files = files if files else {}
         self.cookie = cookie if cookie else {}
