6c867bbe43e9b80368bf8f140069311b53866385
zhaopeng <zhao19870908@qq.com>
2023-09-11 19:19:30 +0800

    下载ting55网站

--- download_files/down_other.py
+++ download_files/down_other.py
@@ -5,6 +5,8 @@
 
 from utils import Req
 from urllib import parse
+import time
+import random
 
 
 class DownloadsOther:
@@ -12,33 +14,53 @@ class DownloadsOther:
     def __init__(self, down_path=None):
         self.down_path = down_path
 
-    def get_download_info_ting55(self, bookId):
+    def get_download_info_ting55(self, book_name, bookId, max_page, folder_path, start_page=None):
         host = "https://m.ting55.com"
-        path = "/glink"
-        payload = {"bookId": bookId, "isPay": 0, "page": 99}
-        headers = {
-            'Accept': 'application/json, text/javascript, */*; q=0.01',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Connection': 'keep-alive',
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Cookie': 'JSESSIONID=563DDDA05E922F84369B876B32FA195A; __bid_n=1889ef9d34ee06fb574207; FPTOKEN=sPDMXaJKNCgQguy7PFWL3CVYaRtAcRaz5Dc7AuKTyMnxM+RSIakC6p84ROvre//VN8xn2gupAq4sK6d/otG1JOCVkv0oUzDrhFoQEoo8F47jfLvWjQmBI5/i+YNCWuElwr2j3aXmXMATYr5K4tSXPks0GjdG4hV0yqJv/Z/zTWp1bDyZPhPccCS37XmpiokCdnCy074+PLIG3bqnsQqLvD3wfU2ZBVPZ3lxRQI5pk3jN46qG6bSkteJ7TvkHup8jBoT0rGLNwFHOH/iONyOSaDM4m17ewgVFOQHiEbHD9ILK71T+mUAgcLEFQMKhDsRvXhAK7G17ASVEFtIQsOqzhIr837d3pJP+AqVlKIHzv9ZVTXTBTzp0h/YvGu0of+XPZmGf9u+NsuBhPs0tHE+9zA==|BxHGUiv/kdoT4Q+3tjS6lobfQYVepJkEQSCAiax5DhA=|10|903ec84034ce319f3a75bbbfbbdd3769; mhting55=3b4e6a48e7ae40b2',
-            'Origin': 'https://m.ting55.com',
-            'Referer': 'https://m.ting55.com/book/13888-166',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.43',
-            'X-Requested-With': 'XMLHttpRequest',
-            'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"macOS"',
-            'xt': 'c6177a5cb078df32ea13f6fa55b44636'
-        }
-        r = Req(url=parse.urljoin(host, path), header=headers, payload=payload)
-        response = r.post()
-        print(response.json())
+        get_audio_url_path = "/glink"
+        start_page = int(start_page) if start_page else 1
+        for i in range(int(max_page)):
+            get_audio_url_payload = {"bookId": bookId, "isPay": 0, "page": i + int(start_page)}
+            get_audio_url_headers = {
+                'Accept': 'application/json, text/javascript, */*; q=0.01',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Cookie': 'JSESSIONID=0A258D3B4B3E1A029A27AF4C10059CA9',
+                'Origin': 'https://m.ting55.com',
+                'Referer': 'https://m.ting55.com/book/13888-166',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-origin',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.43',
+                'X-Requested-With': 'XMLHttpRequest',
+                'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Microsoft Edge";v="114"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"macOS"',
+                'xt': 'c6177a5cb078df32ea13f6fa55b44636'
+            }
+            try:
+                r = Req(url=parse.urljoin(host, get_audio_url_path), header=get_audio_url_headers,
+                        payload=get_audio_url_payload)
+                response = r.post()
+                print(response, response.json())
+            except:
+                r = Req(url=parse.urljoin(host, get_audio_url_path), header=get_audio_url_headers,
+                        payload=get_audio_url_payload)
+                response = r.post()
+                print("第二次尝试", response)
+                print("第二次尝试", response.json())
+
+            audio_url = response.json()["url"] if response.json()["url"] != "" else response.json()["ourl"]
+            audio_format = audio_url.split("/")[-1].split(".")[1]
+            audio_number = audio_url.split("/")[-1].split(".")[0] if audio_url.split("/")[-1].split(".")[
+                0].isdigit() else str(i + int(start_page))
+            dr = Req(url=audio_url)
+            dr.download_get(folder_path,
+                            book_name + ".".join([audio_number.zfill(len(max_page)), audio_format]))
+
+            time.sleep(random.randint(5, 10))
 
 
 if __name__ == "__main__":
     down = DownloadsOther()
-    down.get_download_info_ting55("13888")
+    down.get_download_info_ting55("庆余年", "13679", "751", "/Users/zhaopeng/Personal/有声书/庆余年", "706")
